5a646f26c7d47d93acdeb4f2808d7ea3ef6ee6ce
Lukas Jansen <lukas.jansen1997@yahoo.de>
2015-09-06 22:33:01 +0200

    Added hand giving

--- src/de/zsgn/CardGameSim/players/schwimmen/PlayerSchwimmen.java
+++ src/de/zsgn/CardGameSim/players/schwimmen/PlayerSchwimmen.java
@@ -1,11 +1,19 @@
 package de.zsgn.CardGameSim.players.schwimmen;
 
+import java.util.ArrayList;
+
+import de.zsgn.CardGameSim.cards.Card;
 import de.zsgn.CardGameSim.players.Player;
 
 public abstract class PlayerSchwimmen extends Player {
-
+    protected ArrayList<Card> hand=new ArrayList<Card>();
     protected PlayerSchwimmen(String name) {
         super(name);
+    }
+
+    public void giveHand(ArrayList<Card> hand) {
+        this.hand=hand;
+        
     }
 
 }
